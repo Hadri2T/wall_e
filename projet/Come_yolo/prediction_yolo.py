1d276614e00de1359f6d1bc64f2e4f36d92c1068
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <come@lubrano.net>
2025-06-05 16:04:53 +0200

    saving

--- projet/Come_yolo/prediction_yolo.py
+++ projet/Come_yolo/prediction_yolo.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 import  cv2
 # Load the model
-model = YOLO('runs/detect/train3/weights/best.pt')  # or 'last.pt' for the last checkpoint
+model = YOLO('/Users/comelubrano/code/Hadri2T/wall_e/raw_data/runs_yolo/train/weights/best.pt')  # or 'last.pt' for the last checkpoint
 # Run inference on an image
 # results = model("/Users/dodohellio/code/DodooHellio/Skylight/Sandbox/Roboflow_Model/Horse-pose-estimation.v6i.yolov8/IMG_20250505_101041.jpg")
-results = model("/Users/dodohellio/Downloads/Namibie.jpg")
+results = model("/Users/comelubrano/code/Hadri2T/wall_e/raw_data/test_image/bouteilles-en-plastique-dans-l-océan-bouteille-211244766.webp")
 # Visualize the results
 img = results[0].plot()
 cv2.imshow('YOUPI', img)
