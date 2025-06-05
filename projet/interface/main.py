b89ebf62898af2f9d4ae67b5cc31ff7e516d4293
Hadrien Touchon <hadrien.touchon@sciencespo.fr>
2025-06-05 16:22:05 +0200

    première tentative de main.py

--- projet/interface/main.py
+++ projet/interface/main.py
@@ -2,9 +2,18 @@ from ml_logic import resize_with_padding, resize_bounding_boxes, filter_images
 import pandas as pd
 import os
 from ml_logic.preprocessor import preprocess_and_save_dataset
-from inference import predict  # à intégrer plus tard
-from ml_logic.data import download_from_gcp  
+from ml_logic.model import model
+from ml_logic.data import download_from_gcp
 import pandas as pd
+from params import GCP_PROJECT, LOCAL_DATA_DIR, BUCKET_NAME, TARGET_SIZE
 
 
 if __name__ == "__main__":
+    print("Téléchargement des données prétraitées depuis GCS...")
+    download_from_gcp(prefix_preprocess=GCP_PROJECT, destination_folder=LOCAL_DATA_DIR)
+
+    size_str = str(TARGET_SIZE[0])  # ou "_".join(map(str, TARGET_SIZE)) si tu veux les deux dimensions
+    csv_filename = f"resized_annotations_{size_str}_train.csv"
+    csv_path = os.path.join(LOCAL_DATA_DIR, csv_filename)
+    df = pd.read_csv(csv_path)
+    print(f"{len(df)} annotations chargées")
